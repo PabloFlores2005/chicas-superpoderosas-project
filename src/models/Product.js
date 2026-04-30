b1dab567b1bba1200377d97acaa91cb7fe33db9e
Abigail Melisa Suarez Argote <abigail.suarez@jala.university>
2026-04-30 02:09:26 -0400

    feat: Add initial structure and POST Endpoint

---
+++ src/models/Product.js
@@ -0,0 +1,28 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+}, {
+  tableName: 'products',
+  timestamps: true,
+});
+
+export default Product;
---
+++ src/services/product.service.js
@@ -0,0 +1,15 @@
+import productRepository from '../repositories/product.repository.js';
+
+class ProductService {
+  async createProduct(data) {
+    if (!data.name || data.price === undefined) {
+      throw new Error('Name and price are required');
+    }
+    if (data.price < 0) {
+      throw new Error('Price cannot be negative');
+    }
+    return await productRepository.create(data);
+  }
+}
+
+export default new ProductService();
