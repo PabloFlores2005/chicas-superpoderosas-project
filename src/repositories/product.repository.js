1918d4e8ae29e40cbed098651df028de3813d284
Ic3drago <avilagarciabenjamin@gmail.com>
2026-04-30 16:22:02 -0400

    feat(products): implement GET endpoint to fetch all products

--- src/repositories/product.repository.js
+++ src/repositories/product.repository.js
@@ -4,6 +4,10 @@ class ProductRepository {
   async create(productData) {
     return await Product.create(productData);
   }
+
+  async findAll(){
+    return await Product.findAll();
+  }
 }
 
 export default new ProductRepository();
